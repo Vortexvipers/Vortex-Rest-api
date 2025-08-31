f66a1d72d3407b42bb1f9a8d119f457d71d1451d
Vortexvipers <pribadiarep3@gmail.com>
2025-08-31 12:19:05 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/public/cee5d1d0df379bbe675fbddb84f4992e.jpg" />
+        <link rel="icon" href="/cee5d1d0df379bbe675fbddb84f4992e.jpg" type="image/jpeg" />
       </Head>
       <body>
         <Main />
